8a3e3f6478184d05d61ee7e5fcfacd668a5587d7
cguzman <christian.guzman@bsc.es>
2024-04-23 12:05:18 +0200

    debug bug compiler mn5 with if(true)

--- compile.sh
+++ compile.sh
@@ -1,10 +1,3 @@
-#MONARCH P9 compilation
-
-#module load GCC/7.3.0-2.30
-#module load OpenMPI/3.1.0-GCC-7.3.0-2.30
-#module load CMake/3.15.3-GCCcore-7.3.0
-#module load CUDA/10.1.105-ES
-
 rm -rf build
 mkdir build
 cd build
